511390eb071262fd5fa3dbfa396428b788c89616
Yusuf Turan <y.turan34@gmail.com>
2025-06-14 02:01:59 +0300

    Fix cron expression example and init data

--- src/main/resources/init.sql
+++ src/main/resources/init.sql
@@ -1,4 +1,4 @@
 INSERT INTO job (name, script_path, script_params, cron_expression, next_script1, next_script2, active, last_log, last_exit_code, sequence) VALUES
   ('Daily 12h Stream', 'sh_scripts/run_video_and_stream.sh', '12 --tag lofi --post-twitter', '0 0 0 * * *', NULL, NULL, true, NULL, NULL, 1),
   ('Daily 6h Upload', 'sh_scripts/run_pipeline_and_upload.sh', '6 --tag lofi --post-twitter', '0 0 15 * * *', NULL, NULL, true, NULL, NULL, 2),
-  ('Daily Tweet', 'sh_scripts/post_to_twitter.sh', '--tag lofi', '30 12 * * *', NULL, NULL, true, NULL, NULL, 3);
+  ('Daily Tweet', 'sh_scripts/post_to_twitter.sh', '--tag lofi', '0 30 12 * * *', NULL, NULL, true, NULL, NULL, 3);
