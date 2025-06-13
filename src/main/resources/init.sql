f7aa300bd9733951647b861224ee3c7c2376abd4
Yusuf Turan <y.turan34@gmail.com>
2025-06-14 01:45:42 +0300

    Add job log tracking

--- src/main/resources/init.sql
+++ src/main/resources/init.sql
@@ -1,4 +1,4 @@
-INSERT INTO job (name, script_path, script_params, cron_expression, next_script1, next_script2, active) VALUES
-  ('Daily 12h Stream', 'sh_scripts/run_video_and_stream.sh', '12 --tag lofi --post-twitter', '0 0 0 * * *', NULL, NULL, true),
-  ('Daily 6h Upload', 'sh_scripts/run_pipeline_and_upload.sh', '6 --tag lofi --post-twitter', '0 0 15 * * *', NULL, NULL, true),
-  ('Daily Tweet', 'sh_scripts/post_to_twitter.sh', '--tag lofi', '30 12 * * *', NULL, NULL, true);
+INSERT INTO job (name, script_path, script_params, cron_expression, next_script1, next_script2, active, last_log, last_exit_code) VALUES
+  ('Daily 12h Stream', 'sh_scripts/run_video_and_stream.sh', '12 --tag lofi --post-twitter', '0 0 0 * * *', NULL, NULL, true, NULL, NULL),
+  ('Daily 6h Upload', 'sh_scripts/run_pipeline_and_upload.sh', '6 --tag lofi --post-twitter', '0 0 15 * * *', NULL, NULL, true, NULL, NULL),
+  ('Daily Tweet', 'sh_scripts/post_to_twitter.sh', '--tag lofi', '30 12 * * *', NULL, NULL, true, NULL, NULL);
